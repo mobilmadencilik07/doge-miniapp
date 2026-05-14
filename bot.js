cf9f3f01a44dec267979759a9f74a439321122a5
mobilmadencilik07 <bozkurtbalil97@gmail.com>
2026-05-14 12:10:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,24 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const TOKEN = "8226313005:AAE3Z-mLfWUv-RcnhYJpOi8-jZm9qnS0Pdw";
+const TOKEN = "8226313005:AAFcQdKpDn6ohr3M3f0a79FKRzf_nHRodWA";
 
-const bot = new TelegramBot(TOKEN, { polling:true });
+const bot = new TelegramBot(TOKEN, {
+  polling: true
+});
 
-bot.onText(/\/start/, (msg)=>{
+bot.onText(/\/start/, (msg) => {
 
   bot.sendMessage(
     msg.chat.id,
     "🐶 DOGE MINI APP",
     {
-      reply_markup:{
-        inline_keyboard:[
+      reply_markup: {
+        inline_keyboard: [
           [
             {
-              text:"🚀 OPEN APP",
-              web_app:{
-                url:"https://url:"https://mobilmadencilik07-doge-miniapp.vercel.app/""
+              text: "🚀 OPEN APP",
+              web_app: {
+                url: "https://mobilmadencilik07-doge-miniapp.vercel.app/"
               }
             }
           ]
